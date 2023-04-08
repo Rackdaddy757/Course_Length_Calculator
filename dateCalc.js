3aab780a408e0b36f8dbd1225f8440e44b36affd
Lynnardo Holland <chefnardo@live.com>
2023-04-08 15:34:28 -0400

    added notes

--- dateCalc.js
+++ dateCalc.js
@@ -13,7 +13,10 @@ const c2Days=39;
 const c3Days=53;
 const daySelection= [c1Days, c2Days, c3Days];
 
-let submitButton= document.getElementsByClassName("selection");
+let submitButton= document.getElementById("course1");
+/*Can not use a class reference for a button because the event cannot select one target.
+It is advised to create a for loop that loops through elements of the class and then activate
+the event in that way. */
 
 //function to use today's date to add days
 function dateCalculator (currentDate) {
